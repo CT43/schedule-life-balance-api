764360512f367e7831b6b761ed1c7ec12850afdb
github username <github email address>
2018-11-14 15:42:36 -0700

    add index for act search

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -2,6 +2,10 @@ module Api::V1
   class ActivitiesController < ApplicationController
 
     def index
+      binding.pry
+      @activities = Activity.all
+      @activities.select { |activity| activity.schedule_id === params[:q]}
+      render json: @activities
     end
 
     def create
@@ -21,7 +25,7 @@ module Api::V1
     private
 
       def activity_params
-        params.require(:activity).permit(:name, :duration, :schedule_id, :category_id, :start_time, :end_time, :start_time_min, :end_time_min)
+        params.require(:activity).permit(:name, :duration, :schedule_id, :category_id, :start_time, :end_time, :start_time_min, :end_time_min, :q)
       end
 
   end
